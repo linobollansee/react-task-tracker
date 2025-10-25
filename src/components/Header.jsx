5c745d87220cc1ead033769b45bc1bf82cce1ba0
linobollansee <linobollansee@hotmail.com>
2025-10-25 07:18:30 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,17 @@
+// Import React library for building UI components
 import React from 'react';
-const Header = () => <header><h1>React Task Tracker</h1></header>;
+
+/**
+ * Header component
+ *
+ * This is a simple functional component that renders the header of the app.
+ * It contains a heading displaying the app name.
+ */
+const Header = () => (
+  <header>
+    <h1>React Task Tracker</h1> {/* App title displayed in the header */}
+  </header>
+);
+
+// Export Header component to be used in other parts of the application
 export default Header;
